83c50cc83b49a5e3f4a823e5ee538ca9afe2eb86
Sky <skyengineer001@gmail.com>
2025-02-27 17:03:45 +0400

    Add price calculation logic for shopping cart

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log(`Items (4): $${((2095 * 2) + (799 * 2)) / 100}`);
+console.log(`Shipping & handling: $${9.98}`);
+console.log(`Total before tax: $${(5788 + 998) / 100}`);
+console.log(`Estimated tax (10%): $${Math.round((6786 * 0.1) / 100)}`);
